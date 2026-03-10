f1cf5db26f8ad512988846945c2f5e8beea23808
Mohit <mohitt.tech@gmail.com>
2026-03-10 23:06:57 +0530

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,3 +1,8 @@
 """
 The provided code stub reads an integer, n, from STDIN. For all non-negative integers i < n , print i square.
 """
+
+if __name__ == '__main__':
+    n = int(input())
+for i in range(n):
+    print(i * i)
